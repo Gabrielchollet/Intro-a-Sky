679024b2f343e0b6064b1c5e8964f4ba047e2389
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-07-11 17:29:26 -0300

    Update ep_01.py
    Posicionei as funcoes secundarias apos a main

--- EPs/EPs_python/ep_01.py
+++ EPs/EPs_python/ep_01.py
@@ -1,6 +1,44 @@
 # Simulação simplificada da estação de controle da Skyrats
 import math as np
 
+def main():
+    print("Simulador de Drones Skyrats")
+    
+    drone = input("Qual drone voce esta usando?\n")
+    
+    global posicao
+    posicao = float(input("Posição inicial: "))
+    
+    global altura
+    altura = float(input("Altura inicial: "))
+    
+    global armado
+    armado = False 
+    
+    global voando
+    voando = False
+
+    escolha = 0
+    while escolha != 5:
+        print("O que voce quer fazer com o drone?")
+        print("\t1) Armar o drone")
+        print("\t2) Decolar o drone")
+        print("\t3) Mudar a posição do drone")
+        print("\t4) Pousar o drone")
+        print("\t5) Sair")
+        escolha = int(input(""))
+        if escolha == 1:
+            armado = armar(drone, armado, voando)
+        elif escolha == 2:
+            voando = decolar(drone, armado, voando)
+        elif escolha == 3:
+            mudar_posicao(drone, armado, voando)
+        elif escolha == 4:
+            pousar(drone, armado, voando)
+        else:
+            return 0
+
+
 def armar(drone, armado, voando):
     # Verificar se o drone foi armado
     armado == True
@@ -76,42 +114,5 @@ def pousar(drone, armado, voando):
         armado = False
 
 
-
-
-def main():
-    print("Simulador de Drones Skyrats")
-    
-    drone = input("Qual drone voce esta usando?\n")
-    
-    global posicao
-    posicao = float(input("Posição inicial: "))
-    
-    global altura
-    altura = float(input("Altura inicial: "))
-    
-    global armado
-    armado = False 
-    
-    global voando
-    voando = False
-
-    escolha = 0
-    while escolha != 5:
-        print("O que voce quer fazer com o drone?")
-        print("\t1) Armar o drone")
-        print("\t2) Decolar o drone")
-        print("\t3) Mudar a posição do drone")
-        print("\t4) Pousar o drone")
-        print("\t5) Sair")
-        escolha = int(input(""))
-        if escolha == 1:
-            armado = armar(drone, armado, voando)
-        elif escolha == 2:
-            voando = decolar(drone, armado, voando)
-        elif escolha == 3:
-            mudar_posicao(drone, armado, voando)
-        elif escolha == 4:
-            pousar(drone, armado, voando)
-        else:
-            return 0
 main()
+

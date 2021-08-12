b9ec7c5289b037680bf1ff4ecef4a7fd325dcbb0
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-08-12 03:52:31 -0300

    Feature: Finalizado classe bateria

--- EPs/EP_oo/scripts/bateria.py
+++ EPs/EP_oo/scripts/bateria.py
@@ -1,15 +1,36 @@
+
+
 class Bateria():
     def __init__(self, mah, tempoDeCarregamento):
-        raise NotImplementedError
+        self.mah = mah
+        self.tempoDeCarregamento = tempoDeCarregamento
+        self.carga = 0
+        self.uso
+        self.carregavel
 
     def carregar(self, tempo):
-        raise NotImplementedError
+        if self.carga == self.mah:
+            return False
+        else:
+            self.carga += (self.mah * tempo) / self.tempoDeCarregamento
+            return True
 
     def usar(self, tempo):
-        raise NotImplementedError
+        # tempoDeCarregamento == tempoDeVoo
+        consumo = (self.mah * tempo) / self.tempoDeCarregamento
+        if self.carga > consumo:
+            self.carga -= consumo
+            return True
+        else:
+            return False
 
     def calcula_tempo_de_voo(self):
-        raise NotImplementedError
+        return (self.carga * self.tempoDeCarregamento) / self.mah
 
     def status(self):
-        raise NotImplementedError
+        print("Bateria com capacidade " + str(self.mah) +
+              " e tempo de carregamento " + str(self.tempoDeCarregamento))
+        print("Com carga atual: " + str(self.carga))
+        print("Tempo de voo disponivel: " + str(self.calcula_tempo_de_voo()))
+        print("Esta sendo usada: " + "sim" if self.uso == True else "nao")
+        print("Pode ser carregada no momento: " + str(self.carregar(0)))
